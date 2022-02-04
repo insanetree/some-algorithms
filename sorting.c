f4a5cbf4911c5a7ff1eeaea26e9bd8e240cb7c2c
insanetree <p0p0v1c.41ek54nd4r@gmail.com>
2022-02-05 00:32:33 +0100

    made quick_sort declaration

--- sorting.c
+++ sorting.c
@@ -1,1 +1,7 @@
+#include "sorting.h"
+#include "stack.h"
 
+void quick_sort(void* arr, unsigned cnt, size_t size, int(*cmp)(void*, void*))
+{
+	
+}
--- sorting.h
+++ sorting.h
@@ -1,6 +1,6 @@
 #ifndef _SORTING_H_
 #define _SORTING_H_
 
-
+void quick_sort(void*, unsigned, size_t, int (*cmp)(void*, void*));
 
 #endif //_SORTING_H_
