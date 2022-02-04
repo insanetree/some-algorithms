897f0e5db04c13e7ea67cf677a9b798aa0838eb4
insanetree <p0p0v1c.41ek54nd4r@gmail.com>
2022-02-05 00:52:49 +0100

    implemented quick_sort; yet to implement partitioning and choice of a pivot

--- sorting.c
+++ sorting.c
@@ -1,7 +1,39 @@
 #include "sorting.h"
 #include "stack.h"
 
+void* qs_partition(void* low, void* high, size_t size, int(*cmp)(void*, void*))
+{
+	void* piv = NULL;
+	//TODO
+	return piv;
+}
+
 void quick_sort(void* arr, unsigned cnt, size_t size, int(*cmp)(void*, void*))
 {
+	void* low = arr; //pointer to a first element in array
+	void* high = arr + (cnt-1)*size; //pointer to a last element in array
+	void* mid;
+	stack* bound = stack_init();
+	push(bound, low); //addres of an element copied to stack
+	push(bound, high);
 	
+	//PUSH LOW, THEN HIGH
+	while(bound->capacity != 0)
+	{
+		high = top(bound);
+		pop(bound);
+		low = top(bound);
+		pop(bound);
+		mid = qs_partition(low, high, size, cmp);
+		if(low < mid-size)
+		{
+			push(bound, low);
+			push(bound, mid - size);
+		}
+		if(mid+size < high)
+		{
+			push(bound, mid+size);
+			push(bound, high);
+		}
+	}
 }
--- sorting.h
+++ sorting.h
@@ -1,6 +1,8 @@
 #ifndef _SORTING_H_
 #define _SORTING_H_
 
+#include <stddef.h>
+
 void quick_sort(void*, unsigned, size_t, int (*cmp)(void*, void*));
 
 #endif //_SORTING_H_
